69a311eb374ebf4f9c5ee333c589a54102e154cc
DHATRIREDDY <dhatrireddy007@gmail.com>
2020-07-14 00:07:19 +0530

    storing result in dictionary

--- uc1.sh
+++ uc1.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+declare -A dict
 read -p "Enter a value:" a
 read -p "Enter b value:" b
 read -p "Enter c value:" c
+operation4=$(( ($a%$b)+$c ))
+echo "The operation4 value is:"$operation4
+operation3=$(( $c+($a/$b) ))
+echo "The operation3  value is:"$operation3
 operation1=$(( $a+($b*$c) ))
 echo "The operation1 value is:"$operation1
 operation2=$(( ($a*$b)+$c ))
 echo "The operation2 value is:"$operation2
+dict["op1"]=$operation1
+dict["op2"]=$operation2
+dict["op3"]=$operation3
+dict["op4"]=$operation4
+echo ${dict[@]}
 
