1b74f8e90e47481bd1908a894fe171d31d62d53f
DHATRIREDDY <dhatrireddy007@gmail.com>
2020-07-13 23:14:01 +0530

    added operation4

--- uc1.sh
+++ uc1.sh
@@ -2,6 +2,6 @@
 read -p "Enter a value:" a
 read -p "Enter b value:" b
 read -p "Enter c value:" c
-operation3=$(( $c+($a/$b) ))
-echo "The operation4 value is:"$operation3
+operation4=$(( ($a%$b)+$c ))
+echo "The operation4 value is:"$operation4
 
